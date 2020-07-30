1f651031e24ac37740a98530395238fb9fb4d595
Jeff Shimoda's Work Laptop <jshimoda@gmail.com>
2020-07-30 10:26:33 -0400

    minor fix, also removed recfun.zip

--- course1/week1/RecFun.scala
+++ course1/week1/RecFun.scala
@@ -49,7 +49,7 @@ object RecFun extends RecFunInterface {
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money == 0) 1
     else if (money < 0) 0
-    else if (coins.isEmpty && money > 0) 0
+    else if (coins.isEmpty) 0
     else countChange(money,coins.tail) + countChange(money-coins.head,coins)
   }
 }
